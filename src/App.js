8efcca9e25499b994a9c5d86d6464752cf6e11f3
Arshad Rais <86293309+rais17@users.noreply.github.com>
2023-11-05 16:56:39 +0530

    Closing Sidebar at 640px

--- src/App.js
+++ src/App.js
@@ -5,8 +5,27 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import MainContainer from './Components/MainContainer';
 import Watch from './Components/Watch';
 import SearchPage from './Components/SearchPage';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { closeSidebarAtMobile } from "./Utils/Slices/toggleSlice";
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  const handleResize = () => {
+    console.log("Innerwidth", window.innerWidth);
+    if (window.innerWidth < 640)
+      dispatch(closeSidebarAtMobile());
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, [])
 
   const appRouter = createBrowserRouter([
     {
