06a2e276ddca078a1645fccc3cc9322c4aa7b2c8
Arshad Rais <86293309+rais17@users.noreply.github.com>
2023-11-05 16:58:21 +0530

    Update VideosShimmer.js

--- src/Utils/Shimmer/VideosShimmer.js
+++ src/Utils/Shimmer/VideosShimmer.js
@@ -9,15 +9,16 @@ const VideosShimmer = () => {
         Array.from({ length: 12 }).map((element, index) => {
           return (
             <div
-              class={`${
+              key={index}
+              className={`${
                 sidebar
                   ? `w-full sm:basis-[calc(50%-24px)] at1080:basis-[calc(33%-24px)]`
                   : `w-full sm:basis-[calc(50%-24px)] at1080:basis-[calc(33%-24px)] at1408:basis-[calc(25%-24px)]`
               } animate-pulse flex-col space-y-8 md:flex`}
             >
-              <div class="flex aspect-video w-full items-center justify-center rounded bg-gray-300 dark:bg-[#1f1f1f]">
+              <div className="flex aspect-video w-full items-center justify-center rounded bg-gray-300 dark:bg-[#1f1f1f]">
                 <svg
-                  class="h-10 w-10 text-gray-200 dark:text-gray-700"
+                  className="w-10 h-10 text-gray-200 dark:text-gray-700"
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
@@ -26,12 +27,12 @@ const VideosShimmer = () => {
                   <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
                 </svg>
               </div>
-              <div class="w-full">
-                <div class="mb-3 h-2.5 w-48 rounded-full bg-gray-200 dark:bg-[#1f1f1f]"></div>
-                <div class="mb-2.5 h-2 rounded-full bg-gray-200 dark:bg-[#1f1f1f]"></div>
-                <div class="h-2 max-w-[360px] rounded-full bg-gray-200 dark:bg-[#1f1f1f]"></div>
+              <div className="w-full">
+                <div className="mb-3 h-2.5 w-48 rounded-full bg-gray-200 dark:bg-[#1f1f1f]"></div>
+                <div className="mb-2.5 h-2 rounded-full bg-gray-200 dark:bg-[#1f1f1f]"></div>
+                <div className="h-2 max-w-[360px] rounded-full bg-gray-200 dark:bg-[#1f1f1f]"></div>
               </div>
-              <span class="sr-only">Loading...</span>
+              <span className="sr-only">Loading...</span>
             </div>
           );
         })
